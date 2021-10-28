b6a1f4cd5c484d440c9ff728ad165d15eee1179c
fmascare <fem0022@gmail.com>
2021-10-28 17:45:54 -0500

    found last issue as per PR feedback
    Fixed issue where new messages weren't being added to existing or new convos.

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -104,7 +104,6 @@ export const postMessage = (body) => async (dispatch) => {
     }
 
     sendMessage(data, body);
-    dispatch(setFetchingStatus(true));
   } catch (error) {
     console.error(error);
   }
